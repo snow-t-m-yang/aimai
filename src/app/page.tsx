422802f7eca92dac22d36e1fbd03c4ab368dc24c
Snow Yang <ytm199891@gmail.com>
2023-05-26 17:17:18 +0800

    bugfix: Fix tailwind with src folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
+import { db } from "./lib/db";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default async function Home() {
+  await db.set("hokkaido", "hokkaido");
+
   return <div className="text-7xl">Hokkaido</div>;
 }
