1aa80b53347e6b6ff2a57ecbb13499be1a1b5988
Snow Yang <ytm199891@gmail.com>
2023-06-05 20:41:09 +0800

    add component: Add friend button

--- src/components/AddFriendButton.tsx
+++ src/components/AddFriendButton.tsx
@@ -3,6 +3,11 @@
 import { Button } from "./ui/Button";
 
 const AddFriendButton = () => {
+  const addFriend = async (email: string) => {
+    try {
+    } catch (error) {}
+  };
+
   return (
     <form className="max-w-sm">
       <label htmlFor="email">Add friend by email</label>
