7093cd11c8fa595bec5b5e65602268bdc7636e71
Snow Yang <ytm199891@gmail.com>
2023-06-06 19:48:00 +0800

    feat: Add add friend logic & error handle

--- src/components/AddFriendButton.tsx
+++ src/components/AddFriendButton.tsx
@@ -1,24 +1,69 @@
 "use client";
 
+import { addFriendSchema } from "@/lib/validations/add-friend";
+import axios, { AxiosError } from "axios";
+import { useState } from "react";
+import { z } from "zod";
 import { Button } from "./ui/Button";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 
 const AddFriendButton = () => {
+  const [showSuccessState, setShowSuccessState] = useState<boolean>(false);
+
+  type FormData = z.infer<typeof addFriendSchema>;
+
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(addFriendSchema),
+  });
+
   const addFriend = async (email: string) => {
     try {
-    } catch (error) {}
+      const validatedEmail = addFriendSchema.parse({ email });
+
+      await axios.post("/api/friends/add", {
+        email: validatedEmail,
+      });
+
+      setShowSuccessState(true);
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        setError("email", { message: error.message });
+        return;
+      }
+
+      if (error instanceof AxiosError) {
+        setError("email", { message: error.response?.data });
+        return;
+      }
+
+      setError("email", { message: "Something went wrong." });
+    }
+  };
+
+  const onSubmit = (data: FormData) => {
+    addFriend(data.email);
   };
 
   return (
-    <form className="max-w-sm">
+    <form onSubmit={handleSubmit(onSubmit)} className="max-w-sm">
       <label htmlFor="email">Add friend by email</label>
       <div>
         <input
+          {...register("email")}
           type="text"
           className="block w-full text-black"
           placeholder="Type your email here "
         />
       </div>
       <Button>Add</Button>
+      <p>{errors.email?.message}</p>
+      {showSuccessState ? <p>Friend request sent!</p> : null}
     </form>
   );
 };
--- src/lib/validations/add-friend.ts
+++ src/lib/validations/add-friend.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
 
 export const addFriendSchema = z.object({
-  // email:
+  email: z.string().email(),
 });
