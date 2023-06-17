17eb69aad0e4e00332f214b269d7bcf78d004006
Snow Yang <ytm199891@gmail.com>
2023-06-17 18:03:02 +0800

    feat: add accept friend request

--- src/app/api/friends/accept/route.ts
+++ src/app/api/friends/accept/route.ts
@@ -1,5 +1,6 @@
 import { fetchRedis } from "@/helpers/redis";
 import { authOptions } from "@/lib/auth";
+import { db } from "@/lib/db";
 import { getServerSession } from "next-auth";
 import { z } from "zod";
 
@@ -32,8 +33,25 @@ export async function POST(req: Request) {
       idToAdd,
     );
 
+    if (!hasFriendRequest) {
+      return new Response("No friend request", { status: 400 });
+    }
+
+    // add friend to both users
+    await db.sadd(`user:${session.user.id}:friends`, idToAdd);
+    await db.sadd(`user:${idToAdd}:friends`, session.user.id);
+
+    // await db.srem(`user:${idToAdd}:outbound_friend_requests`, session.user.id);
+
+    // remove friend request
+    await db.srem(`user:${session.user.id}:incoming_friend_requests`, idToAdd);
+
     return new Response("Ok");
   } catch (error) {
-    console.log(error);
+    if (error instanceof z.ZodError) {
+      return new Response("Invalid request payload", { status: 422 });
+    }
+
+    return new Response("Invalid request", { status: 400 });
   }
 }
