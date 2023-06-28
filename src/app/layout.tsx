ff83a784450db35906e227872232af2d2efb519d
Snow Yang <ytm199891@gmail.com>
2023-06-28 12:19:09 +0800

    chore: Polish max width for mobile first

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="text-white min-h-[100dvh] bg-black">
+      <body className="dark max-w-sm w-full mx-auto min-h-[100dvh] bg-black">
         <Providers>{children}</Providers>
       </body>
     </html>
